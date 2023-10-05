41bb7c541b5d1f52a2c72b7aeae59d5a56c2239d
lukichova15 <kateryna.lukichova.ik.2023@lpnu.ua>
2023-10-05 09:28:35 +0300

    first master commit

---
+++ Lab_03_2/Lab_03_2.cpp
@@ -0,0 +1,41 @@
+// Lab_03_2.cpp
+// Лукічова Катерина
+// Лабораторна робота № 3.2
+// Розгалуження, задане формулою: функція з параметрами.
+// Варіант 12
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double a; // вхідний параметр
+	double b; // вхідний параметр
+	double c; // вхідний параметр
+	double F; // результат обчислення виразу
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "x = "; cin >> x;
+
+	// спосіб 1: розгалуження в скороченій формі
+	if (x < 0.6 && b + c != 0)
+		F = a*x*x+b*b+c;
+	if (x > 0.6 && b + c == 0)
+		F = (x - a) / (x - c);
+	if (!(x < 0.6 && b + c != 0) && !(x > 0.6 && b + c == 0))
+		F = x / c + x/a;
+	cout << endl;
+	cout << "1) F = " << F << endl;
+	// спосіб 2: розгалуження в повній формі
+	if (x < 0.6 && b + c != 0)
+		F = a*x*x + b*b + c;
+	else
+	if (x > 0.6 && b + c == 0)
+		F = (x - a) / (x - c);
+	else
+		F = x / c + x / a;
+	cout << "2) F = " << F << endl;
+	cin >> x;
+	return 0;
+}
